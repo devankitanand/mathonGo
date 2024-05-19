a71ddb26fcd7fc6154300553ac197784d6e25ac5
Ankit Anand <devankitanand@gmail.com>
2024-05-20 03:06:20 +0530

    fix

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ require('dotenv').config();
 
 db();
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 
 app.use(express.json());
-app.use('/api', userRoutes);
+app.use(`${process.env.URL}/api`, userRoutes);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
